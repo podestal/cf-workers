fa59aff60f25236774f556377ecdcf9945750d77
podestal <luisrodriguezpodesta@gmail.com>
2025-11-11 09:31:58 -0500

    schema done

--- divine-truth-d328/src/db/schema.ts
+++ divine-truth-d328/src/db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { pgTable, uuid, varchar, integer, timestamp, check } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar, integer, timestamp, check, boolean } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable('users', {
     id: uuid().primaryKey().defaultRandom(),
@@ -12,3 +12,15 @@ export const usersTable = pgTable('users', {
     check('age_check1', sql`${table.age} <= 120`),
     check('age_check2', sql`${table.age} >= 0`),
 ])
+
+export const todosTable = pgTable('todos', {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid().notNull().references(() => usersTable.id {
+        onDelete: 'cascade',
+    }),
+    title: varchar({ length: 500 }).notNull(),
+    description: varchar({ length: 1000 }),
+    completed: boolean().default(false),
+    createdAt: timestamp({ withTimezone: true }).defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).defaultNow(),
+})
