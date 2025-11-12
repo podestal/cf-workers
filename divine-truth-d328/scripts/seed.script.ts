561dd0a6736e8ffd2a0dbb4d14a765fe2573e923
podestal <luisrodriguezpodesta@gmail.com>
2025-11-12 09:43:32 -0500

    adjut seed script

--- divine-truth-d328/scripts/seed.script.ts
+++ divine-truth-d328/scripts/seed.script.ts
@@ -1,8 +1,9 @@
 import * as schema from '../src/db/schema';
 import { db, pool } from '../src/db/db'
-import { seed } from 'drizzle-seed'
+import { reset, seed } from 'drizzle-seed'
 
 export const seedDb = async () => {
+    await reset(db, schema)
     await seed(db, schema).refine((funcs) => ({
         usersTable: {
             columns: {
@@ -11,6 +12,35 @@ export const seedDb = async () => {
             count: 10,
             with: {
                 todosTable: 10
+            }
+        },
+        todosTable: {
+            columns: {
+                title: funcs.valuesFromArray({ 
+                    values: [
+                        'Buy groceries',
+                        'Buy a new phone',
+                        'Read a book',
+                        'Watch a movie',
+                        'Go for a walk',
+                        'Do some exercise',
+                        'Learn a new skill',
+                        'Practice a new language',
+                        'Learn a new skill',
+                    ] 
+                }),
+                description: funcs.valuesFromArray({ 
+                    values: [
+                        'Buy groceries for the week',
+                        'Buy a new phone for the latest model',
+                        'Read a book on a new topic',
+                        'Watch a movie on a new genre',
+                        'Go for a walk in the park',
+                        'Do some exercise to stay fit',
+                        'Learn a new skill to improve my career',
+                        'Practice a new language to communicate with people',
+                    ] 
+                })
             }
         }
     }))
